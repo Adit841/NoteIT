b5ff83fb97d578edba4cf30aadb5b60861b80da9
Aditya Gour <adityagour0841@gmail.com>
2026-03-02 10:01:56 +0530

    added delte and add section

--- script.js
+++ script.js
@@ -1,20 +1,58 @@
 const addBtn = document.querySelector('.addbtn');
 const modal = document.querySelector('.modal');
-const saveBtn = document.querySelector('saveNote');
+const saveBtn = document.querySelector('#saveNote');
 const notesContainer = document.querySelector('.notes');
 
 addBtn.addEventListener('click', () => {
     modal.classList.remove('hidden');
 });
 
-saveBtn.addEventListener('click', () => {
-    const title = document.getElementById('title').value;
-    const desc = document.getAnimations('desc').value;
-    const category = document.getElementById('category').value;
+saveBtn.addEventListener("click", () => {
+  const title = document.getElementById("title").value;
+  const desc = document.getElementById("desc").value;
+  const category = document.getElementById("category").value;
 
-    const date = new Date().toLocaleDateString();
+  const date = new Date().toLocaleDateString();
 
-    const note = document.createElement('div');
+  const note = document.createElement("div");
+  note.classList.add("note-card");
 
-    
-})
+  note.innerHTML = `
+    <div class="card-top">
+      <span class="badge ${category}">
+        ${category}
+      </span>
+
+      <div class="actions">
+        <input type="checkbox">
+        <span class="icon">✏️</span>
+        <span class="delete-btn">🗑️</span>
+      </div>
+    </div>
+
+    <h3>${title}</h3>
+    <p>${desc}</p>
+
+    <div class="date">${date}</div>
+    `;
+
+  notesContainer.appendChild(note);
+
+  // DELETE
+  const deleteBtn = note.querySelector(".delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+  });
+
+  // COMPLETE
+  const checkbox = note.querySelector("input[type='checkbox']");
+
+  checkbox.addEventListener("change", () => {
+    note.classList.toggle("completed");
+  });
+
+  modal.classList.add("hidden");
+});
+
+
