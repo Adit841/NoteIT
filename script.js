bb224a930da8d083fe453e287981a1aa18752898
Aditya Gour <adityagour0841@gmail.com>
2026-03-03 17:17:14 +0530

    adding sopmethings

--- script.js
+++ script.js
@@ -2,83 +2,93 @@ const addBtn = document.querySelector(".addbtn");
 const modal = document.querySelector(".modal");
 const saveBtn = document.querySelector("#saveNote");
 const notesContainer = document.querySelector(".notes");
+const exitBtn = document.querySelector("#exit");
 
-let notes = JSON.parse(localStorage.getItem("notes")) || [];
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
 
-addBtn.addEventListener("click", () => {
-  modal.classList.remove("hidden");
+exitBtn.addEventListener('click', () => {
+  modal.classList.add('hidden');
+})
+addBtn.addEventListener('click',() => {
+  modal.classList.remove('hidden');
 });
-function saveTolocalStorage() {
-  localStorage.setItem("notes", JSON.stringify(notes));
+
+function saveToLocalStorage () {
+  localStorage.setItem('notes', JSON.stringify(notes));
 }
 
-function renderNote(noteData) {
-  const note = document.createElement("div");
-  note.classList.add("note_card");
+function renderNote(noteData){
+  const note = document.createElement('div');
+  note.classList.add('note_card');
 
   note.innerHTML = `
-    <div class="top">
-      <span class="badge ${noteData.category}">
-        ${noteData.category}
+    <div class = "top">
+      <span class = 'badge ${noteData.category}'>
+      ${noteData.category}
       </span>
-
-      <div class="actions">
+      
+      <div class = "actions">
         <input type="checkbox" ${noteData.completed ? "checked" : ""}>
         <span class="icon">✏️</span>
         <span class="delete_btn">🗑️</span>
       </div>
     </div>
-
     <h3>${noteData.title}</h3>
     <p>${noteData.desc}</p>
-    <div class="date">${noteData.date}</div>
+    <div class ="date">${noteData.date}</div>
   `;
 
   notesContainer.appendChild(note);
 
-  const deleteBtn = note.querySelector(".delete_btn");
+  const deleteBtn = note.querySelector('.delete_btn');
 
-  deleteBtn.addEventListener("click", () => {
+  deleteBtn.addEventListener('click',()=>{
     note.remove();
-    notes = notes.filter((n) => n !== noteData);
-    saveTolocalStorage();
+    notes = notes.filter(n => n.id !== noteData.id );
+    saveToLocalStorage();
   });
 
-  const checkbox = note.querySelector("input");
+  const checkbox = note.querySelector('input');
 
-  checkbox.addEventListener("change", () => {
-    note.classList.toggle("completed");
+  checkbox.addEventListener('change', () => {
+    note.classList.toggle('completed');
     noteData.completed = checkbox.checked;
-    saveTolocalStorage();
+    saveToLocalStorage();
+
   });
 
-  if (noteData.completed) {
-    note.classList.add("completed");
+  if(noteData.completed){
+    note.classList.add('completed');
   }
 }
-saveBtn.addEventListener("click", () => {
-  const title = document.getElementById("title").value;
-  const desc = document.getElementById("desc").value;
-  const category = document.getElementById("category").value;
-  const date = new Date().toLocaleDateString();
 
+saveBtn.addEventListener("click", () => {
+  const title = document.getElementById('title').value;
+  const desc = document.getElementById('desc').value;
+  const category = document.getElementById('category').value;
+  const date = new Date().toLocaleDateString();
+  
+  if(title.trim() === "" || desc.trim()===""){
+    alert("please fill all fields");
+    return;
+  }
   const noteData = {
+    id: Date.now(),
     title,
     desc,
     category,
     date,
-    completed: false,
+    completed:false
   };
-
+  
   notes.push(noteData);
-  saveTolocalStorage();
-
+  saveToLocalStorage();
   renderNote(noteData);
+  modal.classList.add('hidden');
 
-  modal.classList.add("hidden");
+  document.getElementById('title').value ="";
+  document.getElementById('desc').value ="";
 
-  document.getElementById("title").value = "";
-  document.getElementById("desc").value = "";
+  
 });
-
 notes.forEach(renderNote);
